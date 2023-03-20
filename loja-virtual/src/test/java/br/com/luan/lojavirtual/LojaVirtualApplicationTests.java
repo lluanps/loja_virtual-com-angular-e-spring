1e2d2f0c62eb0caab3803e9f8434bcacc963510e
Luan Pinheiro <lluanps@gmail.com>
2023-03-20 02:00:22 -0300

    Criação do projeto e configuração do banco de dados postgres

---
+++ loja-virtual/src/test/java/br/com/luan/lojavirtual/LojaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luan.lojavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
