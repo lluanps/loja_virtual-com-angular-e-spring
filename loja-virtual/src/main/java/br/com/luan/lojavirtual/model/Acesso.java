949cd0420e4b651ea845afaa83b536a5c473e397
Luan Pinheiro <lluanps@gmail.com>
2023-03-22 02:48:17 -0300

    Adicionado as classes Endereco, PessoaFisica, PessoaJuridica, Pessoa, Acesso e criado suas tabela

---
+++ loja-virtual/src/main/java/br/com/luan/lojavirtual/model/Acesso.java
@@ -0,0 +1,68 @@
+package br.com.luan.lojavirtual.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Table(name = "acesso")
+@SequenceGenerator(name = "seq_acesso", sequenceName = "seq_acesso",
+allocationSize = 1, initialValue = 1)
+public class Acesso implements Serializable, GrantedAuthority{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_acesso")
+	private Long id;
+	
+	@Column(nullable = false)
+	private String descricao;/*Acesso ex: ROLE_ADMIN ou ROLE_CLIENT*/
+	
+	@Override
+	public String getAuthority() {
+		return this.descricao;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Acesso other = (Acesso) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
