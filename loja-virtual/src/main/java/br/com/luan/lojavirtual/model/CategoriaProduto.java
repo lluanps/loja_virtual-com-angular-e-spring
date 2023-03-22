fd0a869db922314d4558d858181b9825a9fc5747
Luan Pinheiro <lluanps@gmail.com>
2023-03-22 01:33:12 -0300

    adicionado CategoriaProduto

--- loja-virtual/src/main/java/br/com/luan/lojavirtual/model/CategoriaProduto.java
+++ loja-virtual/src/main/java/br/com/luan/lojavirtual/model/CategoriaProduto.java
@@ -12,19 +12,27 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "marca_produto")
-@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto",
+@Table(name = "categoria_produto")
+@SequenceGenerator(name = "seq_categoria_produto", sequenceName = "seq_categoria_produto",
 allocationSize = 1, initialValue = 1)
-public class MarcaProduto implements Serializable {
-
+public class CategoriaProduto implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria_produto")
 	private Long id;
-
+	
 	@Column(nullable = false)
 	private String nomeDesc;
+	
+	public CategoriaProduto() {
+	}
+
+	public CategoriaProduto(Long id, String nomeDesc) {
+		super();
+		this.id = id;
+		this.nomeDesc = nomeDesc;
+	}
 
 	public Long getId() {
 		return id;
@@ -55,7 +63,7 @@ public class MarcaProduto implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		MarcaProduto other = (MarcaProduto) obj;
+		CategoriaProduto other = (CategoriaProduto) obj;
 		return Objects.equals(id, other.id);
 	}
 
