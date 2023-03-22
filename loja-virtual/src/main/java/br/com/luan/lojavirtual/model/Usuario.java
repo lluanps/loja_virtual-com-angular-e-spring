4c742d333b67ec302942e4ec78c20ec25081851c
Luan Pinheiro <lluanps@gmail.com>
2023-03-22 17:06:42 -0300

    Criado tabela Usuario e colocando suas referencias

---
+++ loja-virtual/src/main/java/br/com/luan/lojavirtual/model/Usuario.java
@@ -0,0 +1,98 @@
+package br.com.luan.lojavirtual.model;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.ConstraintMode;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+@Table(name = "usuario")
+@SequenceGenerator(name = "seq_usuario", sequenceName = "seq_usuario",
+allocationSize = 1, initialValue = 1)
+public class Usuario implements UserDetails, Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_usuario")
+	private Long id;
+	
+	@Column(nullable = false)
+	private String login;
+	
+	@Column(nullable = false)
+	private String senha;
+	
+	/*Carrega os acessos apenas quando necessário*/
+	@Temporal(TemporalType.DATE)
+	@JoinTable(name = "usuario")
+	private Date dataAtualSenha;
+
+	@OneToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "usuario_acesso",
+	uniqueConstraints = @UniqueConstraint (columnNames = {"usuario_id", "acesso_id"},
+	name = "unico _acesso_user"),
+	joinColumns = @JoinColumn(name = "usuario_id", referencedColumnName = "id",
+	table = "usuario", unique = false, foreignKey = @ForeignKey(name = "usuario_fk",
+	value = ConstraintMode.CONSTRAINT)), 
+	inverseJoinColumns = @JoinColumn(name = "acesso_id", unique = false, referencedColumnName = "id",
+	table = "acesso", foreignKey = @ForeignKey(name = "acesso_fk", value = ConstraintMode.CONSTRAINT)))
+	private List<Acesso> acessos;
+	
+	/*Autoridades = São acessos, do tipo ROLE_ADMIN, ROLE_SECREATARIO, ROLE_FINANCEIRO*/
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return this.acessos;
+	}
+
+	@Override
+	public String getPassword() {
+		return this.senha;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.login;
+	}
+
+	/*foram colocados metodos recebendo true, para nao dar problema com autentificaçaõ*/
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+	
+}
