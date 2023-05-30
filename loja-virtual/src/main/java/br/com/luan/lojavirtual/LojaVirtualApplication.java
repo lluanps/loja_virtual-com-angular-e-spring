cd1160d40003edaad9ff97c3a86f31acd7ab0031
lluanps <lluanps@gmail.com>
2023-05-30 18:55:32 -0300

    Adicionado configuração na classe main do projeto

--- loja-virtual/src/main/java/br/com/luan/lojavirtual/LojaVirtualApplication.java
+++ loja-virtual/src/main/java/br/com/luan/lojavirtual/LojaVirtualApplication.java
@@ -3,9 +3,15 @@ package br.com.luan.lojavirtual;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EntityScan(basePackages = "br.com.luan.lojavirtual.model")
+@EntityScan(basePackages = "br.com.luan.lojavirtual.model") //mapea classes de model que gera tabelas do banco
+@ComponentScan(basePackages = {"br.*"}) //procura todos os arquivos com 'br.' 
+@EnableJpaRepositories(basePackages = {"br.com.luan.lojavirtual.repository"}) //usado para assegurar que o repository entre na procura
+@EnableTransactionManagement //gerencia transações do banco de dados
 public class LojaVirtualApplication {
 
 	public static void main(String[] args) {
