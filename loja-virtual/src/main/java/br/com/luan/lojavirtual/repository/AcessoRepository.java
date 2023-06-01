bae7dc693fa1a478a70db6bb388b9c0e566778e2
lluanps <lluanps@gmail.com>
2023-05-31 23:56:57 -0300

    Criado busca por descrição e testes de acesso

--- loja-virtual/src/main/java/br/com/luan/lojavirtual/repository/AcessoRepository.java
+++ loja-virtual/src/main/java/br/com/luan/lojavirtual/repository/AcessoRepository.java
@@ -1,6 +1,9 @@
 package br.com.luan.lojavirtual.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,5 +12,8 @@ import br.com.luan.lojavirtual.model.Acesso;
 @Repository
 @Transactional
 public interface AcessoRepository extends JpaRepository<Acesso, Long> {
-
+	
+	@Query("SELECT a FROM Acesso a WHERE UPPER(trim(a.descricao)) LIKE %?1%")
+	List<Acesso> buscaAcessoDescricao(String descricao);
+	
 }
--- loja-virtual/src/test/java/br/com/luan/lojavirtual/LojaVirtualApplicationTests.java
+++ loja-virtual/src/test/java/br/com/luan/lojavirtual/LojaVirtualApplicationTests.java
@@ -1,29 +1,77 @@
 package br.com.luan.lojavirtual;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import br.com.luan.lojavirtual.controller.AcessoController;
 import br.com.luan.lojavirtual.model.Acesso;
 import br.com.luan.lojavirtual.repository.AcessoRepository;
-import br.com.luan.lojavirtual.service.AcessoService;
 
 @SpringBootTest(classes = LojaVirtualApplication.class)
 public class LojaVirtualApplicationTests {
 	
 	@Autowired
-	private AcessoService acessoService;
+	private AcessoController acessoController; 
 	
 	@Autowired
 	private AcessoRepository acessoRepository;
-
+	
+	
 	@Test
-	public void cadastraAcesso() {
+	public void testCadastraAcesso() {
+		
 		Acesso acesso = new Acesso();
 		
 		acesso.setDescricao("ROLE_ADMIN");
 		
-		acessoService.save(acesso);
+		assertEquals(true, acesso.getId() == null);
+
+		/*Gravou no banco de dados*/
+		acesso = acessoController.salvaAcesso(acesso).getBody();
+		
+		assertEquals(true, acesso.getId() > 0);
+		
+		/*Validar dados salvos da forma correta*/
+		assertEquals("ROLE_ADMIN", acesso.getDescricao());
+		
+		/*Teste de carregamento*/
+		
+		Acesso acesso2 = acessoRepository.findById(acesso.getId()).get();
+		
+		assertEquals(acesso.getId(), acesso2.getId());
+		
+		
+		/*Teste de delete*/
+		
+		acessoRepository.deleteById(acesso2.getId());
+		
+		acessoRepository.flush(); /*Roda esse SQL de delete no banco de dados*/
+		
+		Acesso acesso3 = acessoRepository.findById(acesso2.getId()).orElse(null);
+		
+		assertEquals(true, acesso3 == null);
+		
+		
+		/*Teste de query*/
+		
+		acesso = new Acesso();
+		
+		acesso.setDescricao("ROLE_ALUNO");
+		
+		acesso = acessoController.salvaAcesso(acesso).getBody();
+		
+		List<Acesso> acessos = acessoRepository.buscaAcessoDescricao("ALUNO".trim().toUpperCase());
+		
+		assertEquals(1, acessos.size());
+		
+		acessoRepository.deleteById(acesso.getId());
+		
+		
 		
 	}
 	
