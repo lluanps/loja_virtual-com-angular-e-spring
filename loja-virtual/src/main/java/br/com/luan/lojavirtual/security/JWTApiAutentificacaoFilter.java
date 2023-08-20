730ddf16da6f364cd2ac59032e017bb58719d0e5
Luan Pinheiro da Silva <lluanps@gmail.com>
2023-08-20 01:44:16 -0300

    JWTApiAutentificacaoFilter

---
+++ loja-virtual/src/main/java/br/com/luan/lojavirtual/security/JWTApiAutentificacaoFilter.java
@@ -0,0 +1,33 @@
+package br.com.luan.lojavirtual.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import br.com.luan.lojavirtual.security.service.JWTTokenAutentificacaoService;
+
+public class JWTApiAutentificacaoFilter extends GenericFilterBean{
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		/*estabelece a autentificação do usuario*/
+		Authentication authentication = new JWTTokenAutentificacaoService()
+				.getAuthentification((HttpServletRequest)request, (HttpServletResponse)response);
+		
+		/*add processo de autentificação do Spring security*/
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		/*continua com a requisicao, ou chama a api ou bloqueia*/
+		chain.doFilter(request, response);
+	}
+
+}
--- loja-virtual/src/main/java/br/com/luan/lojavirtual/security/WebConfigSecurity.java
+++ loja-virtual/src/main/java/br/com/luan/lojavirtual/security/WebConfigSecurity.java
@@ -1,5 +1,6 @@
 package br.com.luan.lojavirtual.security;
 
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpSessionListener;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,10 +8,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import br.com.luan.lojavirtual.security.service.ImplUserDetailService;
 
@@ -23,6 +28,22 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter implements H
 	private ImplUserDetailService implUserDetailService; 
 	
 	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		/*Ativa proteção do user q n ta validado por token*/
+		http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+			.disable().authorizeRequests().antMatchers("/").permitAll()//usado para accessar a parte incial/principal 
+			.antMatchers("/index").permitAll()
+			.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()//evitar bloqueio de cors do navegador
+			.anyRequest().authenticated().and().logout().logoutSuccessUrl("/index")//redireciona ou retorna o index ao deslogar
+			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))//mapeia o logout do sistema
+			//filtra as requisições
+			.and()
+				.addFilterAfter(new JWTLoginFilter("/login", authenticationManager()),
+					UsernamePasswordAuthenticationFilter.class)
+				.addFilterBefore(new JWTApiAutentificacaoFilter(), UsernamePasswordAuthenticationFilter.class);
+	}
+	
+	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(implUserDetailService).passwordEncoder(new BCryptPasswordEncoder());
 	}
@@ -30,10 +51,10 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter implements H
 	@Override
 	public void configure(WebSecurity webSecurity) {
 		/*Ignorando urls temporariamente*/
-		webSecurity.ignoring()
-		.antMatchers(HttpMethod.POST, "/salvarAcesso")
-		.antMatchers(HttpMethod.GET, "/buscar")
-		.antMatchers(HttpMethod.DELETE, "/delete");
+		//webSecurity.ignoring()
+		//.antMatchers(HttpMethod.POST, "/salvarAcesso")
+		//.antMatchers(HttpMethod.GET, "/buscar")
+		//.antMatchers(HttpMethod.DELETE, "/delete");
 	}
 	
 }
